f7028eeaff4f99553390d9d9f191c952db8e57a4
J12aGuan <96441175+J12aGuan@users.noreply.github.com>
2025-05-28 19:04:59 -0700

    commit

--- data/stockData.py
+++ data/stockData.py
@@ -1,3 +1,20 @@
+import os
 import yfinance as yf
-import pandas as pd
 
+stockList = ["AAPL"]
+
+def download_stock_data(ticker, start="2015-01-01", end="2025-01-01"):
+    # Download historical stock data for a given ticker symbol
+    df = yf.download(ticker, start=start, end=end)
+
+    # Select only the 'Close' price
+    df = df[['Close']].reset_index()
+
+    # Save the DataFrame to a CSV file
+    # Replace the old data with the new data if it already exist (index=False)
+    df.to_csv(f"data/csv/{stock}.csv", index=False)
+
+    print(df)
+
+for stock in stockList:
+    download_stock_data(ticker=stock)
